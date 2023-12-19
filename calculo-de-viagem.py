d514cb6441c8eef0b3385160311ea339d6ef31f2
Douglas Barcellos <douglasbarcellosti@gmail.com>
2023-12-19 16:13:38 -0300

    Desafios iniciais py - Unimed BH

---
+++ calculo-de-viagem.py
@@ -0,0 +1,18 @@
+# DICAS SOBRE PYTHON:
+# FUNÇÃO input(): Ela recebe como parâmetro uma String que será visível ao usuário, 
+# onde geralmente informa que tipo de informação ele está esperando receber;
+# FUNÇÃO print(): Ela é a responsável por imprimir os dados e informar os valores no terminal;
+# MÉTODO split(): permite dividir o conteúdo da variável de acordo com as condições especificadas 
+# em cada parâmetro da função ou com os valores predefinidos por padrão;
+
+# Abaixo segue um exemplo de código que você pode ou não utilizar
+valores = input().split()
+
+# TODO:  Calcule quantidade de litros necessária para realizar a viagem e imprima com TRÊS dígitos após o ponto decimal
+H = int(valores[0])
+V = int(valores[1])
+D = H * V
+
+L = D / 12
+
+print(f'{L: .3f}')
